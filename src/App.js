e2480bb4a070201da9a4991cf31aa7ae8e11862c
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2019-11-28 15:25:57 +0300

    ver 1.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React                          from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Alert}                        from "./components/Alert.js";
+import {Navbar}                       from "./components/Navbar.js";
+import {AlertState}                   from "./context/alert/AlertState.js";
+import {FirebaseState}                from "./context/firebase/FirebaseState.js";
+import {About}                        from "./pages/About.jsx";
+import {Home}                         from "./pages/Home.jsx";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <FirebaseState>
+         <AlertState>
+            <BrowserRouter>
+               <Navbar/>
+               <div className="container pt-4">
+                  <Alert/>
+                  <Switch>
+                     <Route path={'/'} exact component={Home}/>
+                     <Route path={'/about'} component={About}/>
+                  </Switch>
+               </div>
+            </BrowserRouter>
+         </AlertState>
+      </FirebaseState>         
+   );
 }
 
 export default App;
