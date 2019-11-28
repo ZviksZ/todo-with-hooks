29367dadc33ec2c793b2d3ea78b7f3fb1e97aad9
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2019-11-28 15:54:34 +0300

    ver 1.0.1

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Loader = () => (
    <div className='text-center'>
-      <div className="spinner-border" role="status">
+      <div className="spinner-border text-primary" role="status">
          <span className="sr-only">Loading...</span>
       </div>
    </div>
